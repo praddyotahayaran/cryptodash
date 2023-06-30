df166e814ea582300c71cb4426c0312e2df8acc8
Praddyota <praddyotahayaran100@gmail.com>
2023-06-30 13:51:47 +0530

    routing error solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,23 @@ import Transaction from './Pages/Transaction/Transaction';
 import Support from './Pages/Support/Support';
 import Dashboard from './Pages/Dashboard/Dashboard'
 import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+  HashRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
-
-const router = createBrowserRouter([
-  {
-    path: "/cryptodash/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/cryptodash/transaction",
-    element: <Transaction />,
-  },
-  {
-    path: "/cryptodash/support",
-    element: <Support />
-  },
-
-]);
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/transaction' element={<Transaction />} />
+        <Route path='/support' element={<Support />} />
+      </Routes>
+    </HashRouter>
   )
 }
+
 
 export default App
